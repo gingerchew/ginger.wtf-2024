80fc7aaa3220aca023dae22619c5ba8394ccc40d
ginger <26461046+gingerchew@users.noreply.github.com>
2023-10-12 20:08:07 -0400

    Adds initial idea for theme controls

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,3 +5,20 @@ import 'focus-visible'
 
 // Internal Modules
 // import './modules/nav'
+
+
+
+const root = document.documentElement;
+
+if (root.dataset.mode !== 'light' && !localStorage.mode) {
+    const mode = matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    
+    localStorage.mode = mode;
+    root.dataset.mode = mode;
+}
+if (localStorage.theme) {
+    themeSelect.value = localStorage.theme;
+}
+toggleDarkMode.onclick = () => localStorage.mode = root.dataset.mode = document.documentElement.dataset.mode === 'light' ? 'dark' : 'light';
+
+themeSelect.onchange = () => localStorage.theme = root.dataset.theme = themeSelect.value;
