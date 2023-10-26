3320945779406492e52eb87d0bab64a0d563e222
ginger <26461046+gingerchew@users.noreply.github.com>
2023-10-26 17:20:36 -0400

    Adds digital garden, updates css vars

---
+++ src/_data/garden.js
@@ -0,0 +1,8 @@
+
+module.exports = [
+    {
+        title: "Adding View Transitions",
+        link: 'https://daverupert.com/2023/05/getting-started-view-transitions/',
+        description: "Dave Rupert gives a quick guide on how to add View Transitions to your site"
+    }
+]
