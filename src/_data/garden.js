70ed1bd5351fa338d4689a05889ac4117f97f449
ginger <26461046+gingerchew@users.noreply.github.com>
2023-10-27 13:03:22 -0400

    Adds link to the garden

--- src/_data/garden.js
+++ src/_data/garden.js
@@ -4,5 +4,10 @@ module.exports = [
         title: "Adding View Transitions",
         link: 'https://daverupert.com/2023/05/getting-started-view-transitions/',
         description: "Dave Rupert gives a quick guide on how to add View Transitions to your site"
+    },
+    {
+        title: "Privacy Guides",
+        link: "https://www.privacyguides.org/en/",
+        description: "In depth explanation on how to improve your privacy on/with technology"
     }
 ]
