07de960f753270775e38edd5e1339ed584eb1000
ginger <26461046+gingerchew@users.noreply.github.com>
2023-10-31 18:57:31 -0400

    Attempt at solving odd feedbin formatting issue

--- utils/filters.js
+++ utils/filters.js
@@ -1,5 +1,5 @@
 const { DateTime } = require('luxon')
-
+const { JSDOM } = require('jsdom')
 module.exports = {
     dateToFormat: function (date, format) {
         return DateTime.fromJSDate(date, { zone: 'utc' }).toFormat(
@@ -24,5 +24,38 @@ module.exports = {
 
     filterTagList (tags) {
         return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
+    },
+    /**
+     * This is a work around for FeedBin
+     * 
+     * Apparently some styles and elements are not brought over from the content returned by 11ty
+     * these being the direct anchor links, or heading links, or jump links
+     * any <br/> elements
+     * and the new lines after spans in pre>code type components
+     * @param {TheContent} content 
+     * @returns TheContent
+     */
+    fixForFeedbin(content) {
+        const doc = new JSDOM(content);
+
+        doc.window.document.querySelectorAll('a.direct-link').forEach(el => el.remove());
+        doc.window.document.querySelectorAll('br').forEach(el => {
+            el.insertAdjacentHTML('beforebegin', '\n');
+            el.remove();
+        });
+        /*
+        doc.window.document.querySelectorAll('pre[class] > code[class]').forEach(el => {
+            el.querySelectorAll(':scope > span').forEach(el => {
+                el.outerHTML = `<div>${el.outerHTML}</div>`;
+            })
+        })
+        */
+       doc.window.document.querySelectorAll('pre[class > code[class]').forEach(el => {
+        el.style.whiteSpace = 'pre-wrap';
+       })
+        const newContent = doc.window.document.body.innerHTML;
+
+        console.log(newContent);
+        return newContent;
     }
 }
