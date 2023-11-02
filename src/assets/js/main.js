fbec616aa92143841342b17f0660d9217ebdaf66
ginger <26461046+gingerchew@users.noreply.github.com>
2023-11-02 19:15:28 -0400

    Adds better theme generation converts theme select to beacon removes js from darkmodetoggle include

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,2 +1,27 @@
 // Focus Visible Polyfill
 // import 'focus-visible'
+const setCookie = (name) => document.cookie = `${name}; SameSite=Lax; HTTPOnly=true; Secure=true;`;
+$toggleDarkMode.onclick = async ({ target }) => {
+    const mode = target.dataset.mode === 'light' ? 'dark' : 'light';
+    try {
+        navigator.sendBeacon('/style/', new URLSearchParams({
+            mode,
+        }));
+    } catch(e) {
+        console.log('Error: ', e.message);
+    }
+    setCookie('mode='+mode);
+    target.dataset.mode = mode
+}
+$themeSelect.onchange = ({ target }) => {
+    const theme = target.value;
+
+    try {
+        navigator.sendBeacon('/style/', new URLSearchParams({ theme }))
+    } catch(e) {
+        console.log('Error: ', e.message);
+    }
+
+    target.dataset.theme = theme;
+    setCookie('theme='+theme);
+}
