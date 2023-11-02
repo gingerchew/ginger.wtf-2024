4fa5025a58094dd4506bb647531bdaf4863dec78
ginger <26461046+gingerchew@users.noreply.github.com>
2023-11-01 20:41:10 -0400

    Really just trying anything at this point

--- netlify/edge-functions/eleventy-edge.js
+++ netlify/edge-functions/eleventy-edge.js
@@ -17,13 +17,14 @@ export default async (request, context) => {
       ],
     });
 
-    const tCookies = context.cookies.get('theme') ?? 'light';
-    const mCookies = context.cookies.get('mode') ?? 'default';
-
+    
     edge.config((eleventyConfig) => {
       // Add some custom Edge-specific configuration
       // e.g. Fancier json output
       // eleventyConfig.addFilter("json", obj => JSON.stringify(obj, null, 2));
+      const tCookies = context.cookies.get('theme') ?? 'light';
+      const mCookies = context.cookies.get('mode') ?? 'default';
+      
       eleventyConfig.addGlobalData('theme', tCookies);
       eleventyConfig.addGlobalData('mode', mCookies);
     });
