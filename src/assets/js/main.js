da4c6abf5724e8a551a5678610c7bdb4f5626b4c
ginger <26461046+gingerchew@users.noreply.github.com>
2023-11-03 11:44:39 -0400

    Creates/converts some elements to custom elements
    the {emoji} in the footer is now random onConnected
    the featured labels are random onConnected
    // leaving the njk for progressive enhancement
    the darkMode and themeSelect have been converted back into
    web components

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,9 @@
 // Focus Visible Polyfill
 // import 'focus-visible'
-const setCookie = (name) => document.cookie = `${name}; SameSite=Lax; HTTPOnly=true; Secure=true;`,
+const def = (name, el) => customElements.define(name, el),
+    setCookie = (name) => document.cookie = `${name}; SameSite=Lax; HTTPOnly=true; Secure=true;`,
     umtc = () => document.getElementById('$themeColor').content = getComputedStyle(document.documentElement).getPropertyValue('--backgroundColor');
+/*
 document.getElementById('$toggleDarkMode')?.addEventListener('click', ({ target }) => {
     const mode = target.dataset.mode === 'light' ? 'dark' : 'light';
     try {
@@ -13,6 +15,35 @@ document.getElementById('$toggleDarkMode')?.addEventListener('click', ({ target 
     target.dataset.mode = mode
     umtc();
 })
+
+const sendBeacon = (data) => {
+    try {
+        navigator.sendBeacon('/style/', new URLSearchParams(data))
+    } finally {}
+}*/
+
+def('toggle-mode', class extends HTMLElement {
+    constructor() {
+        super()
+        this.btn = this.querySelector('button');
+    }
+
+    set mode(v) {
+        this.dataset.mode = v;
+        setCookie('mode='+v);
+        umtc();
+    }
+    get mode() {
+        return this.dataset.mode;
+    }
+    connectedCallback() {
+        this.btn.addEventListener('click', () => {
+            this.mode = this.mode === 'light' ? 'dark' : 'light';
+            sendBeacon({ mode: this.mode });
+        });
+    }
+})
+/*
 document.getElementById('$themeSelect')?.addEventListener('change', ({ target }) => {
     const theme = target.value;
 
@@ -23,4 +54,52 @@ document.getElementById('$themeSelect')?.addEventListener('change', ({ target })
     target.dataset.theme = theme;
     setCookie('theme='+theme);
     umtc();
+});*/
+
+def('theme-select', class extends HTMLElement {
+    constructor() {
+        super();
+        this.select = this.document.querySelector('select');
+    }
+
+    set theme(v) {
+        this.dataset.theme = v;
+        setCookie('theme='+v);
+        umtc();
+    }
+
+    get theme() {
+        return this.dataset.theme;
+    }
+
+    connectedCallback() {
+        this.select.addEventListener('change', ({ target }) => {
+            this.theme = target.value;
+            sendBeacon({ theme: this.theme });
+        })
+    }
+});
+
+
+
+let emoji = ['🌸','☠️','🤷','😵‍💫','🤡','🍋'],
+    i = Math.floor(Math.random() * emoji.length)
+def('rand-emoji', class extends HTMLElement {
+    constructor() {
+        super()
+    }
+    connectedCallback() {
+        this.textContent = emoji[i];
+    }
+})
+
+const labels = [
+    'Spicyyy!', 'Hot Dog!', 'Woah!', 'Read Me!', 'Yowza!', 'Featured', 'Oh Boy!', 'Gold Star!', 'Whoopee!'
+];
+def('featured-label', class extends HTMLElement {
+    constructor() { super() }
+    connectedCallback() {
+        let i = Math.floor(Math.random() * labels.length);
+        this.textContent = labels[i];
+    }
 })
