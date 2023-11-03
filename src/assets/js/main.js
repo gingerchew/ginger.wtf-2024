40d815711dd5ecf4bc335867ef203522976604f1
ginger <26461046+gingerchew@users.noreply.github.com>
2023-11-03 11:52:51 -0400

    Fixes some js errors

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,25 +2,12 @@
 // import 'focus-visible'
 const def = (name, el) => customElements.define(name, el),
     setCookie = (name) => document.cookie = `${name}; SameSite=Lax; HTTPOnly=true; Secure=true;`,
-    umtc = () => document.getElementById('$themeColor').content = getComputedStyle(document.documentElement).getPropertyValue('--backgroundColor');
-/*
-document.getElementById('$toggleDarkMode')?.addEventListener('click', ({ target }) => {
-    const mode = target.dataset.mode === 'light' ? 'dark' : 'light';
-    try {
-        navigator.sendBeacon('/style/', new URLSearchParams({
-            mode,
-        }));
-    } finally {}
-    setCookie('mode='+mode);
-    target.dataset.mode = mode
-    umtc();
-})
-
-const sendBeacon = (data) => {
-    try {
-        navigator.sendBeacon('/style/', new URLSearchParams(data))
-    } finally {}
-}*/
+    umtc = () => document.getElementById('$themeColor').content = getComputedStyle(document.documentElement).getPropertyValue('--backgroundColor'),
+    sendBeacon = (data) => {
+        try {
+            navigator.sendBeacon('/style/', new URLSearchParams(data))
+        } finally {}
+    };
 
 def('toggle-mode', class extends HTMLElement {
     constructor() {
@@ -42,24 +29,12 @@ def('toggle-mode', class extends HTMLElement {
             sendBeacon({ mode: this.mode });
         });
     }
-})
-/*
-document.getElementById('$themeSelect')?.addEventListener('change', ({ target }) => {
-    const theme = target.value;
-
-    try {
-        navigator.sendBeacon('/style/', new URLSearchParams({ theme }))
-    } finally {}
-
-    target.dataset.theme = theme;
-    setCookie('theme='+theme);
-    umtc();
-});*/
+});
 
 def('theme-select', class extends HTMLElement {
     constructor() {
         super();
-        this.select = this.document.querySelector('select');
+        this.select = this.querySelector('select');
     }
 
     set theme(v) {
@@ -91,7 +66,7 @@ def('rand-emoji', class extends HTMLElement {
     connectedCallback() {
         this.textContent = emoji[i];
     }
-})
+});
 
 const labels = [
     'Spicyyy!', 'Hot Dog!', 'Woah!', 'Read Me!', 'Yowza!', 'Featured', 'Oh Boy!', 'Gold Star!', 'Whoopee!'
@@ -102,4 +77,4 @@ def('featured-label', class extends HTMLElement {
         let i = Math.floor(Math.random() * labels.length);
         this.textContent = labels[i];
     }
-})
+});
