0a41b9458e223ec0a2415ab5db44062339ac03b3
ginger <26461046+gingerchew@users.noreply.github.com>
2023-11-02 21:29:34 -0400

    Cleans up the style switcher code a bit

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,29 +2,25 @@
 // import 'focus-visible'
 const setCookie = (name) => document.cookie = `${name}; SameSite=Lax; HTTPOnly=true; Secure=true;`,
     umtc = () => document.getElementById('$themeColor').content = getComputedStyle(document.documentElement).getPropertyValue('--backgroundColor');
-$toggleDarkMode.onclick = async ({ target }) => {
+document.getElementById('$toggleDarkMode')?.addEventListener('click', ({ target }) => {
     const mode = target.dataset.mode === 'light' ? 'dark' : 'light';
     try {
         navigator.sendBeacon('/style/', new URLSearchParams({
             mode,
         }));
-    } catch(e) {
-        console.log('Error: ', e.message);
-    }
+    } finally {}
     setCookie('mode='+mode);
     target.dataset.mode = mode
     umtc();
-}
-$themeSelect.onchange = ({ target }) => {
+})
+document.getElementById('$themeSelect')?.addEventListener('change', ({ target }) => {
     const theme = target.value;
 
     try {
         navigator.sendBeacon('/style/', new URLSearchParams({ theme }))
-    } catch(e) {
-        console.log('Error: ', e.message);
-    }
+    } finally {}
 
     target.dataset.theme = theme;
     setCookie('theme='+theme);
     umtc();
-}
+})
