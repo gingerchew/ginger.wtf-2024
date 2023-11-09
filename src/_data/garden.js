ee0d017c501e3be7b56917bed69677a4aa82cc65
ginger <26461046+gingerchew@users.noreply.github.com>
2023-11-09 15:37:20 -0500

    Adds 11tybundle.dev garden link

--- src/_data/garden.js
+++ src/_data/garden.js
@@ -9,5 +9,10 @@ module.exports = [
         title: "Privacy Guides",
         link: "https://www.privacyguides.org/en/",
         description: "In depth explanation on how to improve your privacy on/with technology"
+    },
+    {
+        title: "11tybundle.dev",
+        link: "https://11tybundle.dev/",
+        description: "A handy collection of 11ty <i>stuffs</i>, including a firehose RSS feed"
     }
 ]
