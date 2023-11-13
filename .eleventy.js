b965b75d69815cbfa321740564ce4e654f0dee2d
ginger <26461046+gingerchew@users.noreply.github.com>
2023-11-13 11:24:14 -0500

    Fixes proxy path setting for plausible plugin

--- .eleventy.js
+++ .eleventy.js
@@ -38,7 +38,7 @@ module.exports = function (eleventyConfig) {
 	
 	eleventyConfig.addPlugin(EleventyPluginPlausible, {
 		domain: 'ginger.wtf',
-		proxyPath: '/js/script',
+		proxyPath: '/js/script.exclusions',
 		exclude: [ '/style/', '/test' ],
 	});
 
