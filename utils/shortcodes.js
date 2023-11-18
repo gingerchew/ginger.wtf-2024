a65d101a5455a56e0f3c2429b5362bd574d49f14
ginger <26461046+gingerchew@users.noreply.github.com>
2023-11-18 13:17:18 -0500

    Adds profilepic shortcode, wraps author and u-photo in h-card

--- utils/shortcodes.js
+++ utils/shortcodes.js
@@ -1,6 +1,19 @@
 const Image = require('@11ty/eleventy-img');
 
 module.exports = {
+    profilePic: async (src, alt) => {
+        const meta = await Image(src, {
+            width: 300,
+            formats: ['avif'],
+            urlPath: '/images/',
+            outputDir: './_site/public/images/'
+        });
+
+        const { avif } = meta;
+        const data = avif[avif.length - 1];
+
+        return `<img src="${data.url}" width="${data.width}" height="${data.height}" alt="${alt}" decoding="async" loading="lazy" class="u-photo" />`
+    },
     image: async (src, alt, classNames) => {
         const meta = await Image(src, {
             widths: [300, 600],
